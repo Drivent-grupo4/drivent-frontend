1dd44e5941d91283903e83a6083fbe35b82ed77f
RubsRafa <rubiarafaela2000@gmail.com>
2023-05-10 18:22:24 -0300

    feat: add confirmation payment layout

---
+++ src/components/PaymentConfirmation.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import OK from '../assets/images/confirmed.png';
+
+export function PaymentConfirmation() {
+  return (
+    <ConfirmationBox>
+      <img src={OK} alt='confirmation-image' />
+      <div>
+        <h1>Pagamento confirmado!</h1>
+        <h2>Prossiga para escolha de hospedagem e atividades</h2>
+      </div>
+    </ConfirmationBox>
+  );
+}
+
+const ConfirmationBox = styled.div`
+display: flex;
+img{
+    width: 50px;
+    height: 50px;
+}
+div{
+    font-family: Roboto, sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    color: #454545;
+    h1{
+        margin: 8px 0 5px 8px;
+    }
+    h2{
+        margin: 0 8px;
+        font-size: 16px;
+    }
+}
+`;
