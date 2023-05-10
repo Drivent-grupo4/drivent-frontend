851d7f7809436e34e097b6e885c4935ea6904e84
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-05-10 16:00:56 -0300

    chore: adjust ticket

--- src/components/HostingSquare.js
+++ src/components/HostingSquare.js
@@ -1,24 +1,37 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-export default function HostingSquare({ info, index, setTicketPrice, setHotelTypeId, hotelTypeId, setShowTotal, showTotal }) {
-  const noHotel = index === 0;
+export default function HostingSquare({ setTicketPrice, setShowTotal, setTicketTypeId }) {
+  const [selectedStyle, setSelectedStyle] = useState(null);
 
   return (
-    <HostingSquareStyle
-      key={index}
-      selectedStyle={ hotelTypeId === info.id }
-      disabled={ showTotal }
-      onClick={() => {
-        setTicketPrice((prevState) => prevState + (noHotel ? 0 : 250));
-        setHotelTypeId(info.id);
-        setShowTotal(true);
-      }}
-    >
-      <h3>{noHotel ? 'Sem Hotel' : 'Com Hotel'}</h3> <p>+ R$ {noHotel ? '0' : '250'}</p>
-    </HostingSquareStyle>
+    <>
+      <HostingSquareStyle
+        selectedStyle={selectedStyle === 3}
+        onClick={() => {
+          setTicketPrice(250);
+          setShowTotal(true);
+          setTicketTypeId(3);
+          setSelectedStyle(3);
+        }}
+      >
+        <h3>Sem Hotel<p>+ R$ 0</p></h3>
+      </HostingSquareStyle>
+      <HostingSquareStyle
+        selectedStyle={ selectedStyle === 2}
+        onClick={() => {
+          setTicketPrice(600);
+          setShowTotal(true);
+          setTicketTypeId(2);
+          setSelectedStyle(2);
+        }}
+      >
+        <h3>Com Hotel<p>+ R$ 350</p></h3>
+      </HostingSquareStyle>
+    </>
   );
 }
 
 const HostingSquareStyle = styled.button`
-  background: ${({ selectedStyle }) => selectedStyle ? '#ffeed2' : ''} 
+background: ${({ selectedStyle }) => selectedStyle ? '#ffeed2' : '#FFFFFF'} 
 `;
