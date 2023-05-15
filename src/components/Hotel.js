e2bd2085cce34c4c06b2b609aede4fea822f8b4c
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-05-15 15:27:44 -0300

    feat: hotel component

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,55 +1,41 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getHotels } from '../services/hotelApi';
 import useToken from '../hooks/useToken';
 import useHotel from '../hooks/api/useHotel';
+import useRooms from '../hooks/api/useRoom';
+import { getRooms } from '../services/roomApi';
 import { getAccommodationTypes, getAvailability } from './acomodations';
+import { PersonOutline, Person } from 'react-ionicons';
+import HotelPlaceholder from './HotelComponent';
 
 export default function Hotel({ ticket }) {
   const token = useToken();
   const { hotels, loadingHotels } = useHotel();
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [roomId, setRoomId] = useState(null);
+  const [hotelId, setHotelId] = useState(null);
+  const [rooms, setRooms] = useState([]);
+  const [color, setColor] = useState(null);
+  const [selectedIcon, setSelectedIcon] = useState(null);
+  const [showHosting, setShowHosting] = useState(false);
+  const [text, setText] = useState('');
+  const [showButton, setShowButton] = useState(false);
 
-  // async function listHotels() {
-  //   const data = await getHotels(token);
+  async function listRooms(id) {
+    const data = await getRooms(id, token);
+    setRooms(data.Rooms);
 
-  //   return data;
-  // }
+    return rooms;
+  }
 
-  // useEffect(() => {
-  //   listHotels();
-  // }, []);
+  async function sendBooking() {
+    return;
+  }
 
-  //   return ticket ? (
-  //     ticket?.status === 'PAID' ? (
-  //       ticket?.TicketType?.includesHotel === false ? (
-  //         <WarningMessage>
-  //           Sua modalidade de ingresso não inclui hospedagem prossiga para a escolha de atividades
-  //         </WarningMessage>
-  //       ) : (
-  //         <Main>
-  //           <div className="title"> Escolha de hotel e quarto </div>
-  //           <Modality>
-  //             <h2>Primeiro, escolha seu hotel</h2>
-  //             <nav>
-  //               {hotels?.map((hotel) => {
-  //                 <>
-  //                   <HotelContainer>
-  //                     <HotelThumb src={hotel.image} alt="new" />
-  //                     <h4>{hotel.name}</h4>
-  //                   </HotelContainer>
-  //                 </>;
-  //               })}
-  //             </nav>
-  //           </Modality>
-  //         </Main>
-  //       )
-  //     ) : (
-  //       <WarningMessage>Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem</WarningMessage>
-  //     )
-  //   ) : (
-  //     <WarningMessage>Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem</WarningMessage>
-  //   );
-  // }
+  useEffect(() => {
+    listRooms(hotelId);
+  }, [hotelId]);
 
   return (
     <>
@@ -75,50 +61,132 @@ export default function Hotel({ ticket }) {
             <h2>Primeiro, escolha seu hotel</h2>
             <nav>
               {hotels?.map((hotel, index) => (
-                <>
-                  <HotelContainer key={index}>
-                    <HotelThumb src={hotel.image} alt="new" />
-                    <h4>{hotel.name}</h4>
-                    <h3>Tipo de acomodações:</h3>
-                    {/* <p>{getAccommodationTypes(hotel.id)}</p> */}
-                    <h3>Vagas disponiveis:</h3>
-                    {/* <p>{getAvailability(hotel.id)}</p> */}
-                  </HotelContainer>
-                </>
+                <HotelPlaceholder
+                  key={index}
+                  hotel={hotel}
+                  index={index}
+                  hotelId={hotel.id}
+                  setShowHosting={setShowHosting}
+                  listRooms={listRooms}
+                  setHotelId={setHotelId}
+                />
               ))}
             </nav>
+            {showHosting ?
+              <Modality>
+                <h2>Ótima pedida! Agora escolha seu quarto:</h2>
+                <nav>
+                  <MainRooms>
+                    {rooms.map((room, index) => (
+                      <RoomContainer
+                        key={room.id}
+                        selectedRoom={selectedRoom === index}
+                      >
+                        <RoomNumber>{room.name}</RoomNumber>
+                        <IconContainer>
+                          {[...Array(room.capacity).keys()].map((key) => (
+                            selectedIcon === key && selectedRoom === index ?
+                              <Person
+                                key={key}
+                                color={'#FF4791'}
+                                title={''}
+                                height="27px"
+                                width="27px"
+                              />
+                              :
+                              <PersonOutline
+                                key={key}
+                                color={'#000000'}
+                                title={''}
+                                height="27px"
+                                width="27px"
+                                onClick={() => {
+                                  setSelectedRoom(index);
+                                  setSelectedIcon(key);
+                                  setShowButton(true);
+                                }}
+                              />
+                          ))}
+                        </IconContainer>
+                      </RoomContainer>
+                    ))}
+                  </MainRooms>
+                </nav>
+              </Modality>
+              : <></>}
           </Modality>
+          {
+            showButton &&
+            <CloseTicket>
+              <button onClick={() => sendBooking()}>RESERVAR QUARTO</button>
+            </CloseTicket>
+          }
         </Main>
       )}
     </>
   );
 }
-const HotelContainer = styled.div`
-  width: 196px;
-  height: 264px;
-  background: #ebebeb;
-  border-radius: 10px;
-  h4 {
+
+const CloseTicket = styled.div`
+  padding-bottom: 15px;
+  padding-top: 23px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 12px;
+
+  button {
+    width: 100%;
+    height: 35px;
+    word-wrap: initial;
+    max-width: 150px;
+    background-color: #e0e0e0;
+    border-radius: 6px;
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.25);
+    border: 2px inset transparent;
+
     font-family: 'Roboto', sans-serif;
-    font-size: 20px;
-    color: #343434;
-    margin-left: 15px;
-    margin-top: 10px;
-  }
-  h3 {
-    font-size: 13px;
-    font-weight: 700px;
-    margin-left: 15px;
-    margin-top: 10px;
+    font-weight: 400;
+    font-size: 14px;
+    text-align: center;
+    color: #000000;
   }
 `;
 
-const HotelThumb = styled.img`
-  width: 168px;
-  height: 109px;
-  border-radius: 5px;
-  margin-left: 14px;
-  margin-top: 16px;
+const IconContainer = styled.div`
+  margin-top: 9px;
+  margin-right: 10px;
+`;
+
+const MainRooms = styled.div`
+  width: 811px;
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: 205px 205px 205px 205px;
+`;
+
+const RoomContainer = styled.div`
+  width: 190px;
+  height: 45px;
+  border: 1px solid;
+  border-radius: 10px;
+  border-color: #CECECE;
+  margin-bottom: 8px;
+  background: ${({ selectedRoom }) => selectedRoom ? '#ffeed2' : '#FFFFFF'};
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RoomNumber = styled.div`
+  width: 35px;
+  height: 23px;
+  font-family: 'Roboto';
+  font-weight: 700;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 11px;
+  margin-left: 16px;
 `;
 
 const WarningMessage = styled.div`
